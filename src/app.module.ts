0fcd19f57ea592df487f7c79da9dd5343c1e005a
gcoddev <geco.yak77@gmail.com>
2024-07-16 23:09:44 -0400

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,29 +5,31 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Visita } from './visitas/visita.model';
 import { VisitasModule } from './visitas/visitas.module';
 // import { VisitasController } from './visitas/visitas.controller'
+import { config } from 'dotenv'
+config()
 
 @Module({
   imports: [
     SequelizeModule.forRoot({
       dialect: 'mysql',
       // host: 'mysql.gb.stackcp.com',
-      host: 'monorail.proxy.rlwy.net',
-      // host: 'localhost',
+      // host: 'monorail.proxy.rlwy.net',
+      host: process.env.DB_HOST,
 
       // port: 58522,
-      port: 41595,
-      // port: 3306,
+      // port: 41595,
+      port: parseInt(process.env.DB_PORT),
 
       // username: 'portfolio-35303035ab9c',
-      username: 'root',
+      username: process.env.DB_USER,
 
       // password: '[Yakys<3]&g_portfolio;',
-      password: 'pbZVptGgllpVpaXBqCQZBeIYpMnPhUwQ',
-      // password: '',
+      // password: 'pbZVptGgllpVpaXBqCQZBeIYpMnPhUwQ',
+      password: process.env.DB_PASS,
 
       // database: 'portfolio-35303035ab9c',
-      database: 'railway',
-      // database: 'portfolio',
+      // database: 'railway',
+      database: process.env.DB_NAME,
       models: [Visita],
       autoLoadModels: true,
       synchronize: true,
