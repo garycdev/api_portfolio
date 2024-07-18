e8b90cf126f1b4c7a158d5fa2522dc5d4d8327b0
gcoddev <geco.yak77@gmail.com>
2024-07-18 16:33:32 -0400

    device

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.set('trust proxy', true);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
